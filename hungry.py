3bdc2f665294fda3e87c471a0d36ca470eb305de
asdrewq123 <you@example.com>
2021-07-19 16:29:20 +0530

    major chages

--- hungry.py
+++ hungry.py
@@ -2,9 +2,7 @@ hungry=input("are you hugry")
 if hungry=="yes":
     print("eat samosa")
     print("eat pizza")
+    print("burger")
+    print("fries")
 else:
-    thirsty=input("are you thirsty")
-    if thirsty=="yes":
-        print("drink water")
-     else:
-         print("eat")
+    print("do your homework")
